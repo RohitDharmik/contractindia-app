8d35bef15d5e27fca5050c02918912a26b76f945
Rohit Dharmik <rohit1234dharmik@gmail.com>
2026-07-13 20:05:23 +0530

    update

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,7 @@
 import { useAuthStore } from '@/store/authStore';
 
 export function useAuth() {
+  
   const user = useAuthStore((s) => s.user);
   const accessToken = useAuthStore((s) => s.accessToken);
   return {
